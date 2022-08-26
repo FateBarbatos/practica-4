2d51bc854537dd1b0042f1cc8f9ee9178f9693a4
Fate <Fate@DESKTOP-QKB1521>
2022-08-26 09:24:17 +0200

    Agregar archivos de proyecto.

---
+++ practica 4.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace practica_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[5];
+            array1[3] = 8;
+            array1[0] = 15;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.WriteLine(i + " " + array1[i]);
+            }
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = i;
+            }
+            Console.WriteLine(" ");
+            foreach (int row in array1) {
+                Console.WriteLine(row);
+            }
+
+            Console.WriteLine(" ");
+            string[] paises = new string[5] {"El Salvador","Mexico","Chile","Colombia","Venezuela"};
+            foreach(string pai in paises)
+            {
+                Console.WriteLine(pai);
+            }
+        }
+    }
+}
